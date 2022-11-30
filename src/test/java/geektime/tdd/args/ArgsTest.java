4e41677f5e1542aa31e758552e34af2e0f4396b6
lixiaoping <17773406760@189.cn>
2022-11-30 12:34:03 +0800

    [李小平] #N/A feat: 07｜TDD中的测试（3）：集成测试还是单元测试？:结课

--- src/test/java/geektime/tdd/args/ArgsTest.java
+++ src/test/java/geektime/tdd/args/ArgsTest.java
@@ -1,11 +1,17 @@
 package geektime.tdd.args;
 
+import static geektime.tdd.args.OptionParsers.bool;
+import static geektime.tdd.args.OptionParsers.unary;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import geektime.tdd.args.annotation.Option;
 import geektime.tdd.args.exception.LackOptionException;
 import geektime.tdd.args.exception.LackParserException;
+import java.util.Map;
+import java.util.function.Function;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
@@ -37,6 +43,22 @@ class ArgsTest {
       @Option("d") String directory){}
 
   @Test
+  @SuppressWarnings("unchecked")
+  void should_parse_multi_options_behavior() {
+    Function<String, Integer> mockParse1 = mock(Function.class);
+    Function<String, String> mockParse2 = mock(Function.class);
+    final Map<Class<?>, OptionParser<?>> register = Map.of(
+        boolean.class, bool(),
+        int.class, unary(0, mockParse1),
+        String.class, unary("", mockParse2));
+
+    Args args = new Args(register, "-l", "-p", "8088", "-d", "/usr/logs");
+    args.parse(MultiOptions.class);
+    verify(mockParse1).apply("8088");
+    verify(mockParse2).apply("/usr/logs");
+  }
+
+  @Test
   void should_throw_lack_option_exception_if_option_annotation_not_present() {
     LackOptionException exception = assertThrows(LackOptionException.class,
         () -> Args.parse(MultiOptionsLackOption.class,
@@ -58,6 +80,7 @@ class ArgsTest {
             "-l", "-p", "8088", "-d", "/usr/logs"));
     assertThat(exception.getOption()).contains("port");
   }
+
 
   public record MultiOptionsLackParser(
       @Option("l")boolean logging,
